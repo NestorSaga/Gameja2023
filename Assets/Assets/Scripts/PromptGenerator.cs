bc3e9f947e1b2d546bdb20fae0358d7eb5496dc7
Yeray Bartolomé <49511617+YerayBartolome@users.noreply.github.com>
2023-01-02 19:19:55 +0100

    xD

--- Assets/Assets/Scripts/PromptGenerator.cs
+++ Assets/Assets/Scripts/PromptGenerator.cs
@@ -1,28 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using System.IO;
 
 public class PromptGenerator : MonoBehaviour
 {
+    [SerializeField] TMP_Text _promptUI;
+    [SerializeField] PromptData _promptData;
 
-    void Start()
+    void Update()
     {
-        ReadCSV();
-    }
-
-    void ReadCSV()
-    {
-        string[] Lines = System.IO.File.ReadAllLines("/* YOUR_PATH   */");
-        //string[] Columns= Lines[/*   INDEX  */].Split(';');
-        for (int i=0; i<=Lines.Length-1; i++)
+        if (Input.GetKeyDown(KeyCode.Space)) 
         {
-            Debug.Log(Lines[i]);
+            GeneratePrompt();
         }
     }
 
-    public string GeneratePrompt()
+    public void GeneratePrompt()
     {
-        return null;
+        _promptUI.text = _promptData._greetings[Random.Range(0, _promptData._greetings.Length - 1)] + " "
+                        + _promptData._identity[Random.Range(0, _promptData._identity.Length - 1)] + " "
+                        + _promptData._institution[Random.Range(0, _promptData._institution.Length - 1)] + " "
+                        + _promptData._idea[Random.Range(0, _promptData._idea.Length - 1)] + " "
+                        + _promptData._investment[Random.Range(0, _promptData._investment.Length - 1)];                        
     }
 }
